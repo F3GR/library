aaa65a9db15219abcc255bec50b0010ae5c64971
F3GR <egmver.2@gmail.com>
2023-07-06 23:58:12 +0400

    added book constructor

--- sourceCode.js
+++ sourceCode.js
@@ -1,8 +1,31 @@
+const selectedRows = document.querySelectorAll('tbody tr');
+const selectedTitles = document.querySelectorAll('td.title');
+const selectedAuthors = document.querySelectorAll('td.author');
+const selectedPages = document.querySelectorAll('td.pages');
+const selectedStatus = document.querySelectorAll('#read-status');
+
+console.dir(selectedRows);
+console.dir(selectedTitles);
+console.dir(selectedAuthors);
+console.dir(selectedPages);
+console.dir(selectedStatus);
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
 let myLibrary = [];
 
-function Book() {
-  
+for (let i = 0; i < selectedRows.length; i++) {
+    let obj = new Book(selectedTitles[i].textContent, selectedAuthors[i].textContent, 
+        selectedPages[i].textContent, selectedStatus[i].textContent);
+    myLibrary += obj;
 }
+
+console.log(myLibrary);
 
 function addBookToLibrary() {
   
